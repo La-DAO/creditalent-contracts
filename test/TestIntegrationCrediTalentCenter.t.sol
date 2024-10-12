605c2e08237b9eda6e5c823af20ccebb3fb8a7f0
DCota <32775237+DaigaroCota@users.noreply.github.com>
2024-10-12 09:57:19 -0700

    fix: console contract name

--- test/TestIntegrationCrediTalentCenter.t.sol
+++ test/TestIntegrationCrediTalentCenter.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {VmSafe} from "forge-std/StdUtils.sol";
-import {console} from "forge-std/Console.sol";
+import {console} from "forge-std/console.sol";
 import {CreditTalentCenter} from "../src/CreditTalentCenter.sol";
 import {CreditPoints} from "../src/CreditPoints.sol";
 import {MockToken} from "./mocks/MockToken.sol";
